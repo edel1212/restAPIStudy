5fe525b1400b6736854a2f46e8a9c410da3a4422
yoo <edel1212@naver.com>
2024-06-16 21:00:35 +0900

    properties mapping

--- src/main/java/com/yoo/restAPI/common/AppProperties.java
+++ src/main/java/com/yoo/restAPI/common/AppProperties.java
@@ -1,4 +1,18 @@
 package com.yoo.restAPI.common;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "my-app")
+@Getter
+@Setter
 public class AppProperties {
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
 }
